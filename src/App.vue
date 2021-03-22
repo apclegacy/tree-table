ff49d82eeb2f6cfefc9e026c237daaf762ed13e4
alexpetercharles <hi@alexpetercharles.ch>
2021-03-22 20:28:48 +0100

    feat: added background component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import Background from '@/components/Background.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    Background,
   },
 });
 </script>
 
+<template>
+  <div>
+    <background />
+  </div>
+</template>
+
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Background.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Background',
+});
+</script>
+
+<template>
+  <div class="sketch-container" />
+</template>
+
+<style lang="scss" scoped>
+.sketch-container {
+  height: 100vh;
+  width: 100vw;
+  background-color: aqua;
+}
+</style>
