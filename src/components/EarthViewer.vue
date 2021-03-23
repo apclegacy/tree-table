0b7b736e2f620e99b2e2f9ab571ead190817bf4c
alexpetercharles <hi@alexpetercharles.ch>
2021-03-23 12:02:21 +0100

    refactor: minor refactoring and commenting

--- src/components/EarthViewer.vue
+++ src/components/EarthViewer.vue
@@ -5,14 +5,17 @@ import useSketches from '@/modules/useSketches';
 export default defineComponent({
   name: 'EarthViewer',
   setup() {
+    // is a reference to a html element. the ref() function wrappes
+    // the content into a reactive object which tells vue to rerender the dom
+    // as soon as the value changes. thus reactive.
     const sketchContainer = ref({} as HTMLElement);
 
-    onMounted(() => {
-      if (sketchContainer.value) {
-        useSketches(sketchContainer.value);
-      }
-    });
+    // onMounted is a lifecycle event which is hooked right after the Whole Component
+    // including the dome is created and inserted into the app
+    // we need this event because before onMounted the HtmlElement does not exist.
+    onMounted(() => useSketches(sketchContainer.value));
 
+    // return the ref to the virtual dom
     return {
       sketchContainer,
     };
