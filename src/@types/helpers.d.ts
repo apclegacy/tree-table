2bbe108b6a4edec0443f0f67610661aaa0c76068
alexpetercharles <hi@alexpetercharles.ch>
2021-03-24 11:54:14 +0100

    feat: easycam and helper function

---
+++ src/@types/helpers.d.ts
@@ -0,0 +1,3 @@
+declare module '@/helpers/addScreenPosition' {
+  export default addScreenPositionFunction;
+}
--- src/sketches/defaultSketch.ts
+++ src/sketches/defaultSketch.ts
@@ -1,16 +1,22 @@
-import P5 from 'p5';
+import { p5 } from 'p5';
+
+import addScreenPosition from '@/helpers/addScreenPosition';
 
 // this is a function which is called with height and width and returns
 // another function with the parameter p as P5 (callback).
 // This is the Sketch Function which is called by the new P5 instance.
 // Use everything processing related with p.
-const defaultSketch = (height: number, width: number) => ((p: P5) => {
+const defaultSketch = (height: number, width: number) => ((p: p5) => {
   p.setup = () => {
-    p.createCanvas(width, height);
+    p.createCanvas(width, height, p.WEBGL);
+    document.oncontextmenu = () => false;
+    p.createEasyCam();
+    addScreenPosition(p);
   };
   p.draw = () => {
     p.background(220);
-    p.ellipse(50, 50, 80, 80);
+    p.lights();
+    p.box(200);
   };
 });
 
