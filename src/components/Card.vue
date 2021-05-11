4b3c44ee05c9bc9d7790bb86926183675c659e77
lexiboi <hi@alexpetercharles.ch>
2021-05-11 19:02:22 +0200

    refactor: playbutton component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,13 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 
+import PlayButton from '@/components/PlayButton.vue';
+
 export default defineComponent({
   name: 'Card',
+  components: {
+    PlayButton,
+  },
   props: {
     card: {},
     category: {
@@ -85,47 +90,7 @@ export default defineComponent({
                   :class="media.title"
                   playsinline />
                 <span class="play" @click="playVideo(media.title)" :class="`play-${media.title}`">
-                  <svg width="81" height="81" viewBox="0 0 81 81" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g opacity="0.8">
-                    <circle cx="40.0156"
-                      cy="40.4219"
-                      r="38.5" fill="#303030" fill-opacity="0.7" stroke="#00FF75" stroke-width="3"/>
-                    </g>
-                    <path
-                      d="M37.0047 33.8557L45.527 41.7754L37.0047 49.695L37.0047 33.8557Z"
-                      fill="white" stroke="white" stroke-width="3" stroke-linecap="round"/>
-                    <defs>
-                    <filter id="filter0_bii"
-                      x="-49.9844" y="-49.5781"
-                      width="180" height="180"
-                      filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feGaussianBlur in="BackgroundImage" stdDeviation="25"/>
-                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur"/>
-                    <feBlend mode="normal"
-                      in="SourceGraphic"
-                      in2="effect1_backgroundBlur" result="shape"/>
-                    <feColorMatrix in="SourceAlpha"
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dx="1" dy="1"/>
-                    <feGaussianBlur stdDeviation="1"/>
-                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-                    <feColorMatrix type="matrix"
-                      values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.4 0"/>
-                    <feBlend mode="normal" in2="shape" result="effect2_innerShadow"/>
-                    <feColorMatrix in="SourceAlpha"
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dx="-1" dy="-1"/>
-                    <feGaussianBlur stdDeviation="0.5"/>
-                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
-                    <feColorMatrix type="matrix"
-                      values="0 0 0 0 0.45098 0 0 0 0 0.45098 0 0 0 0 0.45098 0 0 0 0.5 0"/>
-                    <feBlend mode="normal" in2="effect2_innerShadow" result="effect3_innerShadow"/>
-                    </filter>
-                    </defs>
-                  </svg>
+                  <play-button />
                 </span>
               </div>
             </div>
@@ -276,6 +241,8 @@ export default defineComponent({
           scroll-snap-type: y mandatory;
 
           background: none;
+
+          display: block;
           .item, img, .video, video {
             width: 100%;
             object-fit: contain;
@@ -298,6 +265,7 @@ export default defineComponent({
 
           .scroll-indicator-list {
             position: sticky;
+
             top: 0;
             left: 0;
             width: 5%;
